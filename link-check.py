702e573969cc79b5e6fb44109c2bfcc0b65ab755
Michael Brackett <34899426+MLJBrackett@users.noreply.github.com>
2020-10-06 23:15:18 -0400

    added json output support

--- link-check.py
+++ link-check.py
@@ -10,11 +10,11 @@ parser = argparse.ArgumentParser(description="link-check is a broken link identi
 parser.add_argument('-v',"--version", action='store_true', help="Returns the current version of tool")
 parser.add_argument('-f','--file',help="Checks the given file in the current directory for urls (-f htmls.txt)", metavar='\b')
 parser.add_argument('-r','--redirect',help="Checks the given file in the current directory for urls and allows for redirecting of urls (-r htmls.txt)", metavar="\b")
+parser.add_argument('-j','--json', nargs='*')
 
 args = parser.parse_args()
 
-version = 0.1
-foundUrls = []
+version = 0.2
 
 # If no arguements print help
 if len(sys.argv) == 1:
@@ -42,14 +42,21 @@ def urlCheck():
                     r = requests.head(url[0],timeout=1.5)
                 elif args.redirect:
                     r = requests.head(url[0],timeout=1.5,allow_redirects=True)
-                if r.status_code in range(200,299):
-                    print(Fore.GREEN + url[0],r.status_code,' GOOD')
-                elif r.status_code in range(400,599):
-                    print(Fore.RED + url[0],r.status_code,' CLIENT/SERVER ISSUE')
-                elif r.status_code in range(300,399):
-                    print(Fore.YELLOW + url[0],r.status_code,' REDIRECT')
+                if args.json is not None:
+                    jsonObj = {
+                        "url": url,
+                        "status": r.status_code
+                    }
+                    jsonArr.append(jsonObj)
                 else:
-                    print(Fore.WHITE + url[0],r.status_code,' UNKNOWN')
+                    if r.status_code in range(200,299):
+                        print(Fore.GREEN + url[0],r.status_code,' GOOD')
+                    elif r.status_code in range(400,599):
+                        print(Fore.RED + url[0],r.status_code,' CLIENT/SERVER ISSUE')
+                    elif r.status_code in range(300,399):
+                        print(Fore.YELLOW + url[0],r.status_code,' REDIRECT')
+                    else:
+                        print(Fore.WHITE + url[0],r.status_code,' UNKNOWN')
             except requests.exceptions.RequestException:
                 print(Fore.RED + url[0],"TIMEOUT")
     print(Style.RESET_ALL)
@@ -57,4 +64,11 @@ def urlCheck():
 if args.version:
     print(Fore.GREEN+"Link"+Fore.RED+" Check",Style.RESET_ALL,"v.",version)
 elif args.file or args.redirect:
-    urlParse()
+    foundUrls = []
+    if args.json is not None:
+        jsonArr = []
+        print(Fore.GREEN,'JSON Object being created...',Style.RESET_ALL)
+        urlParse()
+        print(jsonArr)
+    else: 
+        urlParse()
